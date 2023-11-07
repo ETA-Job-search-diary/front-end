8f6db90fa7bc2cadcab96457c601f4a04d4f6804
Nanii <breadnanii@gmail.com>
2023-11-08 05:15:17 +0900

    FIX: Float Button 위치 수정

--- src/components/common/FloatNewButton.tsx
+++ src/components/common/FloatNewButton.tsx
@@ -3,10 +3,10 @@ import Icon from '@/assets/Icon';
 interface FloatNewButtonProps {
   onClick: () => void;
 }
-//TODO: 그림자 체크 (위에만 적용되는지 다시 한번 확인 필요)
+
 const FloatNewButton = ({ onClick }: FloatNewButtonProps) => {
   return (
-    <div className="fixed bottom-2 w-20 h-20 bg-white flex justify-center items-center rounded-full shadow-tab">
+    <div className="absolute bottom-4 w-20 h-20 bg-white flex justify-center items-center rounded-full shadow-tab">
       <button
         onClick={onClick}
         className="bg-primary500 rounded-full w-16 h-16 m-auto"
