415c94d66e97b760972fb12a27bde943e72e8979
Nanii <breadnanii@gmail.com>
2023-11-08 20:26:40 +0900

    STYLE: NavBar 적용을 위한 스타일 수정

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -7,13 +7,10 @@ const BackButton = () => {
   const router = useRouter();
 
   return (
-    <button
-      onClick={() => router.back()}
-      className="w-full h-12 web:h-[70px] px-[20px]"
-    >
+    <button onClick={() => router.back()} className="w-full h-full px-[20px]">
       <Icon
         name="arrowleft"
-        className="stroke-black800 w-4 h-4 web:w-6 web:h-6"
+        className="stroke-black900 w-4 h-4 web:w-6 web:h-6 hover:scale-105 transition-all"
       />
     </button>
   );
