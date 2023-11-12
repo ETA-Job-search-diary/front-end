916a57c283b387ada0cf8fcd0c6c075fa08aa28d
Nanii <breadnanii@gmail.com>
2023-11-12 13:10:17 +0900

    CHORE: Toast 우측 하단 배치

--- src/components/list/SubScheduleTitle.tsx
+++ src/components/list/SubScheduleTitle.tsx
@@ -8,11 +8,7 @@ const SubScheduleTitle = ({ label, count }: SubScheduleTitleProps) => {
     <div className="flex gap-[10px] text-sm web:text-md">
       <span className="text-black900 font-bold">{label} 일정</span>
       <span className="text-black500 flex ">
-        총
-        <span className="text-primary500 font-bold inline-block w-3">
-          {count}
-        </span>
-        건
+        총<span className="text-primary500 font-bold">{count}</span>건
       </span>
     </div>
   );
