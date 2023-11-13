d5f90445f73d34596d7ef8c94246d6a7e159d46b
Nanii <breadnanii@gmail.com>
2023-11-13 14:11:49 +0900

    FEAT: Detail 컴포넌트 분리
    - form font size 수정
    - form padding 수정
    - detail -> Item 컴포넌트로 분리
    - 서비스 소개 내용 추가

--- src/components/signin/MyAccount.tsx
+++ src/components/signin/MyAccount.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-import TeamImage from '../../../public/images/team-eta.png';
 import { createPortal } from 'react-dom';
 import NavBar from '../common/NavBar';
 import BackButton from '../navbar/BackButton';
@@ -9,7 +7,7 @@ import Icon from '@/assets/Icon';
 import useDisableBodyScroll from '@/hook/useDisableBodyScroll';
 import { getProviderByEmail } from '@/service/signin';
 import Link from 'next/link';
-import { SUPPORT_FORM } from '@/constants/service';
+import { SERVICE_DESCRIPTION, SUPPORT_FORM } from '@/constants/service';
 import { redirect } from 'next/navigation';
 import { useState } from 'react';
 import Alert, { AlertType } from '../common/Alert';
@@ -88,9 +86,8 @@ const MyAccount = ({ session, onClose }: MyAccountProps) => {
           </div>
         </div>
         <div className="flex flex-col items-center px-1 py-3 web:px-3 web:py-5 gap-3">
-          {/*  //!!TODO: 서비스 소개 나오면 수정
           <Link
-            href={'https://www.naver.com'}
+            href={SERVICE_DESCRIPTION}
             rel="noopener noreferrer"
             target="_blank"
             className="w-full flex items-center gap-3 p-3 border-b border-black100"
@@ -100,21 +97,9 @@ const MyAccount = ({ session, onClose }: MyAccountProps) => {
               className="w-3.5 h-3 web:w-5 web:h-4 stroke-black300"
             />
             <span className="text-xs web:text-md text-black900">
-              서비스소개
+              서비스 소개
             </span>
-          </Link> */}
-          <button
-            className="w-full flex items-center gap-3 p-3 border-b border-black100"
-            onClick={handleServiceToast}
-          >
-            <Icon
-              name="message"
-              className="w-3.5 h-3 web:w-5 web:h-4 stroke-black300"
-            />
-            <span className="text-xs web:text-md text-black900">
-              서비스소개
-            </span>
-          </button>
+          </Link>
           <Link
             href={SUPPORT_FORM}
             rel="noopener noreferrer"
@@ -142,7 +127,7 @@ const MyAccount = ({ session, onClose }: MyAccountProps) => {
         </div>
         <Icon
           name="teamETA"
-          className="absolute bottom-12 right-7 web:right-10 web:w-[212px] web:h-[155px] h-[119px] w-40"
+          className="absolute bottom-[4rem] right-7 web:right-10 web:w-[212px] web:h-[155px] h-[119px] w-40"
         />
         {isOpen && (
           <Alert
