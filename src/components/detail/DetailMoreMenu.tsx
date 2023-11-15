c5a81d5f58672a9b2fbb710ee81d867ef97b9b0a
Nanii <breadnanii@gmail.com>
2023-11-15 19:03:40 +0900

    FEAT: 수정 기능 구현

--- src/components/detail/DetailMoreMenu.tsx
+++ src/components/detail/DetailMoreMenu.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import { useToast } from '../ui/use-toast';
-import { BASE_URL } from '@/constants/service';
 import { useState } from 'react';
 import Icon from '@/assets/Icon';
 import {
@@ -12,68 +10,43 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import Alert, { AlertType } from '../common/Alert';
-import axios from 'axios';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import { deleteSchedule } from '@/service/schedule';
 
-type Menu = 'edit' | 'delete';
+enum Menu {
+  edit = '수정',
+  delete = '삭제',
+}
 
 interface DetailMoreMenuProps {
   scheduleId: string;
 }
 
 const DetailMoreMenu = ({ scheduleId }: DetailMoreMenuProps) => {
-  const { toast } = useToast();
   const router = useRouter();
   const { data: session } = useSession();
   const token = session?.user.accessToken;
 
   const [isOpen, setIsOpen] = useState(false);
-  const [message, setMessage] = useState<Menu>();
-
-  const handleEdit = (value: Menu) => {
-    setMessage(value);
-    openMenu();
-  };
-  const handleDelete = (value: Menu) => {
-    setMessage(value);
-    openMenu();
-  };
 
   const openMenu = () => setIsOpen(true);
   const handleCloseMenu = () => setIsOpen(false);
 
   const handleEditConfirm = () => {
-    toast({
-      title: '지금은 수정이 어려워요 🥹',
-      description: '잠시만 기다려주세요!',
-    });
+    handleCloseMenu();
+    router.push(`/edit/${scheduleId}`);
   };
 
   const handleDeleteConfirm = () => {
     if (!token) return;
-    axios
-      .delete(`${BASE_URL}/schedules/${scheduleId}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
+
+    deleteSchedule(scheduleId, token)
       .then(() => {
         handleCloseMenu();
         router.push('/list');
       })
       .catch((err) => console.log(err));
-  };
-
-  const MenuItem = {
-    edit: {
-      text: '수정',
-      onClick: handleEdit,
-    },
-    delete: {
-      text: '삭제',
-      onClick: handleDelete,
-    },
   };
 
   return (
@@ -88,41 +61,36 @@ const DetailMoreMenu = ({ scheduleId }: DetailMoreMenuProps) => {
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
           <DropdownMenuItem
-            onClick={() => MenuItem.edit.onClick('edit')}
+            onClick={handleEditConfirm}
             className="flex justify-center items-center gap-2"
           >
             <Icon name={'edit'} className="w-4 h-4 stroke-black600 fill-none" />
-            <span className="text-xxs web:text-sm">
-              {MenuItem.edit.text}하기
-            </span>
+            <span className="text-xxs web:text-sm">{Menu.edit}하기</span>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
           <DropdownMenuItem
-            onClick={() => MenuItem.edit.onClick('delete')}
+            onClick={openMenu}
             className="flex justify-center items-center gap-2"
           >
             <Icon
               name={'delete'}
               className="w-4 h-4 stroke-black600 fill-none"
             />
-            <span className="text-xxs web:text-sm">
-              {MenuItem.delete.text}하기
-            </span>
+            <span className="text-xxs web:text-sm">{Menu.delete}하기</span>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
-      {isOpen && message && (
+      {isOpen && (
         <Alert
-          message={`등록된 일정을 ${AlertType[message]}할까요?`}
+          message={`등록된 일정을 ${Menu.delete}할까요?`}
           type={[
             {
               value: AlertType.cancel,
               onClick: handleCloseMenu,
             },
             {
-              value: AlertType[message],
-              onClick:
-                message === 'edit' ? handleEditConfirm : handleDeleteConfirm,
+              value: AlertType.delete,
+              onClick: handleDeleteConfirm,
             },
           ]}
           onClose={handleCloseMenu}
