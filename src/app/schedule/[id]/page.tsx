6349ed465dc9c67f4b1b2f264f6d0da2778908ef
Nanii <breadnanii@gmail.com>
2023-11-29 03:30:31 +0900

    CHORE: app icon 수정
    - detail page metadata 추가
    - app icon 수정

--- src/app/schedule/[id]/page.tsx
+++ src/app/schedule/[id]/page.tsx
@@ -4,6 +4,8 @@ import { authOptions } from '@/lib/authOptions';
 import { getScheduleBy } from '@/service/schedule';
 import { getServerSession } from 'next-auth';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+import { getFormattedISODateTime } from '@/service/date';
 
 interface SchedulePageProps {
   params: {
@@ -16,7 +18,6 @@ export default async function SchedulePage({
 }: SchedulePageProps) {
   const session = await getServerSession(authOptions);
   const token = session?.user.accessToken;
-
   if (!id || !token) return notFound();
   const data = await getScheduleBy(id, token);
 
@@ -26,4 +27,23 @@ export default async function SchedulePage({
       <Detail {...data} />
     </>
   );
+}
+
+export async function generateMetadata({
+  params: { id },
+}: SchedulePageProps): Promise<Metadata> {
+  const session = await getServerSession(authOptions);
+  const name = session?.user.name;
+  const token = session?.user.accessToken;
+  if (!id || !token) return notFound();
+  const data = await getScheduleBy(id, token);
+
+  return {
+    title: `${name}님의 일정 - ${data?.title}`,
+    description: `회사 : ${data?.company} | 직무 : ${
+      data?.position
+    } | 전형단계 : ${data?.step} | 일정 : ${
+      getFormattedISODateTime(data?.date).date
+    }`,
+  };
 }
