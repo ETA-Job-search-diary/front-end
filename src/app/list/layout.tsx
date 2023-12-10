3dc7fcce65e9d0bf109a6e91630aeef2e828fb2b
Nanii <breadnanii@gmail.com>
2023-12-11 03:09:57 +0900

    CHORE: mobile pt env 설정 추가

--- src/app/list/layout.tsx
+++ src/app/list/layout.tsx
@@ -1,5 +1,11 @@
 import CheckProvider from '@/context/CheckProvider';
+import { Metadata } from 'next';
 import { ReactNode } from 'react';
+
+export const metadata: Metadata = {
+  title: '다가오는 일정',
+  description: '다가오는 일정을 확인해보세요. 당신의 취뽀를 응원합니다:)',
+};
 
 interface ListPagelayoutProps {
   children: ReactNode;
--- src/app/schedule/[id]/layout.tsx
+++ src/app/schedule/[id]/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import { ReactNode } from 'react';
+
+export const metadata: Metadata = {
+  title: '상세 일정',
+  description: '상세 일정을 확인해보세요.',
+};
 
 interface DetaillayoutProps {
   children: ReactNode;
