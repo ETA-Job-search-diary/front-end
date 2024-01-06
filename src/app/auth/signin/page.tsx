6ce732ee2f61c0e29b2cbf8eb9b3e10dd7dc4249
Nanii <breadnanii@gmail.com>
2024-01-06 23:47:09 +0900

    FEAT: auth middle ware 설정
    - my, new 페이지 접근시 token이 없을경우 redirect auth

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -4,10 +4,15 @@ import { getProviders } from 'next-auth/react';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
-import { PRIVACY_POLICY, TERMS_OF_SERVICE } from '@/constants/service';
 import NavBar from '@/components/common/NavBar';
 import BackButton from '@/components/navbar/BackButton';
+import { PRIVACY_POLICY, TERMS_OF_SERVICE } from '@/constants/service';
 import { getToken } from '@/service/token';
+import { Viewport } from 'next';
+
+export const viewport: Viewport = {
+  themeColor: 'var(--app-white)',
+};
 
 interface SignInProps {
   searchParams: {
@@ -29,13 +34,13 @@ export default async function SingInPage({
       <section className="h-[calc(100vh-70px)] w-full overflow-x-hidden px-page">
         <div className="relative top-[10%]">
           <div className="flex flex-col gap-28 web:gap-32">
-            <div className="text-black-900 flex flex-col gap-6">
+            <div className="flex flex-col gap-6 text-black-900">
               <p className="text-1.5 font-bold xs:text-1.1">
                 <span className="text-primary-500">취준로그</span>
                 는 로그인 후<br />
                 이용이 가능해요
               </p>
-              <p className="text-black-700 text-1.1 xs:text-0.85">
+              <p className="text-1.1 text-black-700 xs:text-0.85">
                 간편한 SNS 로그인 후<br />
                 취준 일정을 쉽고 편하게 관리해보세요!
               </p>
@@ -51,7 +56,7 @@ export default async function SingInPage({
               />
             </div>
           </div>
-          <p className="text-black-400 flex w-full items-center justify-center gap-2 py-7">
+          <p className="flex w-full items-center justify-center gap-2 py-7 text-black-400">
             <Icon name="alert" className="h-4 w-4 leading-4" />
             <span className="text-0.7 xs:text-0.6 web:text-0.8">
               로그인 시{' '}
@@ -59,7 +64,7 @@ export default async function SingInPage({
                 href={TERMS_OF_SERVICE}
                 rel="noopener noreferrer"
                 target="_blank"
-                className="border-black-500 text-black-500 border-b font-medium"
+                className="border-b border-black-500 font-medium text-black-500"
               >
                 이용약관
               </Link>
@@ -68,7 +73,7 @@ export default async function SingInPage({
                 href={PRIVACY_POLICY}
                 rel="noopener noreferrer"
                 target="_blank"
-                className="border-black-500 text-black-500 border-b font-medium"
+                className="border-b border-black-500 font-medium text-black-500"
               >
                 개인정보 처리 방침
               </Link>
--- src/components/common/TabBar.tsx
+++ src/components/common/TabBar.tsx
@@ -1,19 +1,12 @@
 'use client';
 
-import useSession from '@/hook/useSession';
-import { usePathname, useRouter } from 'next/navigation';
-import FloatNewButton from '../new/FloatNewButton';
+import Icon from '@/assets/Icon';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import LinkButton from './LinkButton';
 
 const TabBar = () => {
   const pathname = usePathname();
-  const { push } = useRouter();
-  const { token } = useSession();
-
-  const handleLoginClick = () => {
-    if (token) push('/new');
-    else push('/auth/signin');
-  };
 
   if (
     pathname === '/auth/signin' ||
@@ -27,7 +20,18 @@ const TabBar = () => {
     <nav className="fixed bottom-0 z-40 mx-auto h-20 w-full min-w-280 max-w-500 bg-white shadow-tab">
       <div className="relative grid h-full w-full grid-cols-3 place-items-center pb-6 web:pb-4">
         <LinkButton path="home" />
-        <FloatNewButton onClick={handleLoginClick} />
+        <div className="absolute bottom-3.5 flex h-20 w-20 items-center justify-center rounded-full bg-white shadow-tab">
+          <Link
+            href="/new"
+            className="group z-20 m-auto flex h-16 w-16 items-center justify-center rounded-full bg-primary-500 hover:drop-shadow-md"
+          >
+            <Icon
+              aria-label="=new-button"
+              name="edit"
+              className="h-8 w-8 fill-none stroke-white transition-all group-hover:scale-105"
+            />
+          </Link>
+        </div>
         <LinkButton path="list" />
       </div>
       <div className="absolute bottom-0 grid h-full w-full place-items-center">
