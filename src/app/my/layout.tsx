c6a64791d1a3d7cf8c02e834469d8a112b5f1dc4
Nanii <breadnanii@gmail.com>
2024-01-18 14:17:52 +0900

    STYLE: Home calendar 중앙 정렬

--- src/app/my/layout.tsx
+++ src/app/my/layout.tsx
@@ -15,7 +15,7 @@ interface MyLayoutProps {
 
 const MyLayout = ({ children }: MyLayoutProps) => {
   return (
-    <section className="fixed top-0 h-full min-h-screen w-full min-w-280 max-w-500 overflow-y-auto bg-gray-100 pt-safe-top scrollbar-none web:static">
+    <section className="fixed top-0 h-full min-h-screen w-full min-w-280 max-w-500 overflow-y-auto bg-gray-100 scrollbar-none web:static">
       {children}
     </section>
   );
